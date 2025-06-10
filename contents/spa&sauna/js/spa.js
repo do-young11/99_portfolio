9c8e8a91ee3f856692155dd6a88c4bcc5958e0fc
do-young11 <sdsd362@naver.com>
2025-06-10 10:08:23 +0900

    add

--- contents/spa&sauna/js/spa.js
+++ contents/spa&sauna/js/spa.js
@@ -1,6 +1,6 @@
 //spa.js
 
-/* var pageCount = 0;
+var pageCount = 0;
 var total;
 
 //스크롤상태
@@ -37,66 +37,69 @@ $(function () {
             delta = -evt.detail;
         };
 
-        if (delta < 0) {
-            pageCount++;
-            if (pageCount === total) pageCount = total - 1;
-            boxC++;
-            if (boxC === count) boxC = count - 1;
-        } else {
-            pageCount--;
-            if (pageCount === -1) pageCount = 0;
-        };
-
-        var pageH = $('.page').eq(pageCount).offset().top;
-        console.log('페이지 길이' + pageH);
+        var list = $('.page').eq(pageCount).find('.wrap-box');
 
         //wrap-box의 슬라이드 이동거리
         var boxW = $('.wrap-box .spa-box').width();
         console.log('box 너비' + boxW);
 
-        $('html, body').animate({
-            scrollTop: pageH + 'px'
-
-        }, 800, function () {
-            stat = 0;
-
-            //.spa-box 스크롤
-            $('.ani1').on('mousewheel DOMMouseScroll', function () {
-                if (stat === 1) return false;
-                stat = 1;
-
-                if (delta < 0) {
+        if (list) {
+            if (delta < 0) { // 아래로 스크롤
+                if (boxC < count) {
                     boxC++;
-                    
                     $('.wrap-box').animate({
                         right: '+=' + boxW
-                    }, 1000, function () {
+                    }, 800, function () {
                         stat = 0;
-                        if (boxC === count) boxC = count - 1;
-                        console.log(boxC);
                     });
-
                 } else {
+                    // 마지막 spa-box까지 갔을 경우 다음 페이지로 스크롤
+                    pageCount++;
+                    if (pageCount >= total) pageCount = total - 1;
+                    scrollToPage(pageCount);
+                    $('.wrap-box').css('right','-'+50+'%')
+                }
+            } else { // 위로 스크롤
+                if (boxC > 0) {
                     boxC--;
-                    console.log(boxC+'2');
-                    
-                    if (boxC === -1) boxC = 0;
-                    
-                    $('.wrap-box').animate({
-                        right: '-=' + boxW
-                    }, 1000, function () {
+                    $('.spa-list .wrap-box').animate({
+                        right: '-=' + boxW + 'px'
+                    }, 800, function () {
                         stat = 0;
                     });
-                };
-            });
+                } else {
+                    // 첫 spa-box에서 위로 스크롤 시 이전 페이지
+                    pageCount--;
+                    if (pageCount < 0) pageCount = 0;
+                    scrollToPage(pageCount);
+                }
+            }
+        } else {
+            if (delta < 0) {
+                pageCount++;
+                if (pageCount >= total) pageCount = total - 1;
+            } else {
+                pageCount--;
+                if (pageCount < 0) pageCount = 0;
+            }
+            scrollToPage(pageCount);
+        }
+       
+        
+        var pageH = $('.page').eq(pageCount).offset().top;
+        console.log('페이지 길이' + pageH);
 
+        $('html, body').animate({
+            scrollTop: pageH + 'px'
+        }, 800, function () {
+            stat = 0;
         });
 
     });
 
-}); */
+});
 
-$(function () {
+/* $(function () {
     var stat = 0;
     var pageCount = 0;
     var total = $('.page').length;
@@ -117,10 +120,10 @@ $(function () {
             delta = -evt.detail;
         }
 
-        var isAni1 = $('.page').eq(pageCount).hasClass('.spa-list');
+        var list = $('.page').eq(pageCount).hasClass('.spa-list');
 
         // ani1 섹션일 경우: 슬라이드 먼저
-        if (isAni1) {
+        if (list) {
             if (delta < 0) { // 아래로 스크롤
                 if (boxC < count - 1) {
                     boxC++;
@@ -173,3 +176,4 @@ $(function () {
         });
     }
 });
+ */
