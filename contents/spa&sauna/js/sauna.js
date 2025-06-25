05d3cddfd4cb97d6195b2804b0847a15856801c8
do-young11 <sdsd362@naver.com>
2025-06-25 19:30:26 +0900

    추가

--- contents/spa&sauna/js/sauna.js
+++ contents/spa&sauna/js/sauna.js
@@ -1,10 +1,5 @@
 //sauna.js
 
-var pageCount = 0;
-var total;
-
-//스크롤상태
-var stat = 0;
 
 $(function () {
     logo();
@@ -12,41 +7,27 @@ $(function () {
     $('.imgs img:first').fadeIn(800);
     $('.imgs img:last').delay(1000).fadeIn(800);
 
-    total = $('.page').length;
-    console.log(total);
+   
+     gsap.registerPlugin(ScrollTrigger);
 
-    $(document).on('mousewheel DOMMouseScroll', function () {
-        if (stat === 1) return false;
-        stat = 1;
+    const sections = gsap.utils.toArray(".sauna-box");
 
-        var evt = window.event;
+    const totalWidth = window.outerWidth;
+    console.log(totalWidth);
 
-        var delta = evt.wheelDelta ? evt.wheelDelta : evt.detail;
-        console.log(delta);
+    var moveW = $('.sauna-box').outerWidth();
+    console.log(moveW);
 
-        if (/Firefox/i.test(navigator.userAgent)) {
-            delta = -evt.detail;
-        };       
-        
-        if (delta < 0) {
-            pageCount++;
-            if (pageCount === total) pageCount = total - 1;
-        } else {
-            pageCount--;
-            if (pageCount === -1) pageCount = 0;
-        };
-
-        var pageH = $('.page').eq(pageCount).offset().top;
-        console.log('페이지 길이' + pageH);
-
-        $('html, body').animate({
-            scrollTop: pageH + 'px'
-        }, 800, function () {
-            stat = 0;
-        });
-
+    gsap.to(".wrap-box", { //가로로 이동할 요소
+        x: () => -moveW*3, //이동거리
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".sauna-list", //스크롤이 시작될 요소
+            start: "top top", //시작지점
+            end: () => totalWidth, //끝지점
+            scrub: true, //애니메이션 부드럽게 움직임
+            pin: '.sauna-list', //스크롤 동안 페이지 고정
+            anticipatePin: 1 //레이아웃 튐 현상 방지
+        }
     });
-
-
-
 });
