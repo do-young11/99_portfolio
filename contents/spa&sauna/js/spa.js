1be1d76119e7bff975a8c29e72ba7abe9fd3ddb1
do-young11 <sdsd362@naver.com>
2025-06-26 13:37:40 +0900

    포폴 보강

--- contents/spa&sauna/js/spa.js
+++ contents/spa&sauna/js/spa.js
@@ -1,6 +1,17 @@
 //spa.js
 
+// 스크롤에 따른 header 디자인 변경 샘플!
+/* $(window).scroll(function(){
+    var scTop = $(this).scrollTop();
+    var headerHeight = $('header').height(); //outerHeight()
 
+    if (scTop > headerHeight){
+        $('header').addClass();
+    } else {
+        $('header').removeClass();
+    }
+});
+ */
 $(function () {
     logo();
 
