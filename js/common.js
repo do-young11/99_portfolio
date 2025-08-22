a5d5ce457bed6669319d0d0d444d3ba57b0e201f
do-young11 <sdsd362@naver.com>
2025-08-22 16:51:22 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 
 // 로고 기본 버전
 function logo (){
-    $('.logo a img').attr('src','img/logo.png');
+    $('.logo a img').attr('src','/99_portfolio/img/logo.png');
     $('.gnb a').css({
         color: '#3E3E3B '
     });
@@ -13,7 +13,7 @@ function logo (){
 
 //로고 화이트버전
 function logoW (){
-    $('.logo a img').attr('src','img/logo_w.png');
+    $('.logo a img').attr('src','/99_portfolio/img/logo_w.png');
     $('.gnb a').css({
         color: '#f5f5f2'
     });
